f5841b768fb9ad951757cd02569c6f3e307cd379
lzl2040 <2040171586@qq.com>
2020-12-03 20:49:11 +0800

    polish homepage

--- js/clickFunction.js
+++ js/clickFunction.js
@@ -1,3 +1,4 @@
+//这是点击跳转界面的事件
 var home = document.getElementsByClassName("home");
 var growth = document.getElementsByClassName("growth");
 var daily = document.getElementsByClassName("daily");
@@ -34,5 +35,24 @@ for(var i = 0;i<about.length;i++)
     about[i].onclick = function()
     {
         alert("尚未开发!");
+    }
+}
+//这是点击触发事件
+var homePage = document.getElementsByClassName("homepage")[0];
+var lis = homePage.getElementsByTagName("li");
+var lis_as = homePage.getElementsByTagName("a");
+for(var i = 0;i<lis.length;i++)
+{
+    lis[i].num=i;
+    lis_as[i].num=i;
+    lis[i].onmouseover = function()
+    {
+        for(var j = 0;j<lis.length;j++)
+        {
+            lis[j].style.backgroundColor="rgba(62, 157, 255, .8)";
+            lis_as[j].style.color = "";
+        }
+        lis[this.num].style.backgroundColor="rgba(37,38,255,.8)";
+        lis_as[this.num].style.color = "red";
     }
 }
