dc759bff4681369646e0ed8a61ce7ce11143f162
lzl2040 <2040171586@qq.com>
2020-12-04 16:33:30 +0800

    add pic

---
+++ js/splitPage.js
@@ -0,0 +1,14 @@
+//这是实现分页代码
+var add = document.getElementsByClassName("add")[0];
+add.onmouseover = function()
+{
+    add.style.backgroundColor ="rgba(76,71,76,.7)";
+}
+add.onmouseout = function()
+{
+    add.style.backgroundColor="";
+}
+add.onclick = function()
+{
+    
+}
